ac329fd32678685710d89b5fa998d914d3419249
oinowe <37719618+oinowe@users.noreply.github.com>
2020-11-25 21:33:54 -0800

    Cambio minimo al GET principal

--- index_mongodb.js
+++ index_mongodb.js
@@ -19,7 +19,7 @@ app.use('/students', studentRoutes);
 
 // Initializer
 app.get('/', (req, res) => {
-    res.send('API Proyecto Maestria');
+    res.send('API Proyecto Maestria Node.js');
 });
 
 // Connect to DB
